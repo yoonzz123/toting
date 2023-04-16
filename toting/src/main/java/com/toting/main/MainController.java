c6bce94fbe48f0cf5af587367f72be28343c61e1
toting <yoonzz123@gmail.com>
2023-04-16 16:37:55 +0900

    test-cimmit

--- toting/src/main/java/com/toting/main/MainController.java
+++ toting/src/main/java/com/toting/main/MainController.java
@@ -8,22 +8,13 @@ public class MainController {
 
     @RequestMapping("/")
     public String main(){
-
-
         System.out.println("123123123");
-        System.out.println("123123123");
-        System.out.println("123123123");
-        System.out.println("123123123");
-        System.out.println("123123123");
-
-
-
         return "index";
     }
 
     @RequestMapping("/home")
     public String home(){
-
+        System.out.println("3333333");
         return "home";
     }
 }
